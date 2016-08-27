3ce60ec3ae3ae7f9128313db53be324f32fe2e16
Junior Silva Souza <estrutura.pilha@gmail.com>
2016-08-27 14:12:38 -0400

    Adicionando Arquivos de hibernate.

---
+++ sistemasRoupas/src/main/java/com/tads4/sistemasroupas/App.java
@@ -0,0 +1,13 @@
+package com.tads4.sistemasroupas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
