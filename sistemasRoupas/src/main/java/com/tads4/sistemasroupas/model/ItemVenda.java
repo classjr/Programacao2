4d8d160739c6db09c3395b3c90b126863a73cdb8
robsonpereiradasilva <robson.silva@novaandradina.org>
2016-08-31 15:40:53 -0300

    Add files via upload

---
+++ sistemasRoupas/src/main/java/com/tads4/sistemasroupas/model/ItemVenda.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tads4.sistemasroupas.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author Robson
+ */
+public class ItemVenda {
+    //Produto produto;
+    private int codigoItemVenda;
+    private int quantidade;
+    private double total;
+    
+   
+            
+    
+}
---
+++ sistemasRoupas/src/main/java/com/tads4/sistemasroupas/model/Venda.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tads4.sistemasroupas.model;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+  import javax.persistence.Column;
+  import javax.persistence.Entity;
+  import javax.persistence.Id;
+  import javax.persistence.Table;
+
+
+/**
+ *
+ * @author Robson
+ */
+@Entity
+@Table(name = "Venda")
+public class Venda {
+    @codigoVenda 
+    @GeneratedValue
+    @Column(name=codigoVenda)
+    private long codigoVenda;
+    @TemporalType.TIMESTAMP
+    @Column(name=dataVenda)
+    private Calendar dataVenda;
+    
+    
+    
+    
+    
+}
