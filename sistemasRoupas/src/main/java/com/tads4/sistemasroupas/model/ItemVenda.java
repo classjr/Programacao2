dfd564426d673bc01d1f8925e01110f18a4977a3
robsonpereiradasilva <robson.silva@novaandradina.org>
2016-09-02 19:03:32 -0300

    Adicionado repositorio javax.persistence no arquivo pom Adicionado metodos get e setters nas classe venda e itemVenda Adicionado os annotations nas classses venda e itemVenda

--- sistemasRoupas/src/main/java/com/tads4/sistemasroupas/model/ItemVenda.java
+++ sistemasRoupas/src/main/java/com/tads4/sistemasroupas/model/ItemVenda.java
@@ -5,17 +5,35 @@
  */
 package com.tads4.sistemasroupas.model;
 
+import java.util.Calendar;
 import java.util.List;
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  *
  * @author Robson
  */
+@Entity
+@Table(name="ItemVenda")
 public class ItemVenda {
-    //Produto produto;
+    @Id
+    @GeneratedValue
+    @Column(name="codigoItem")
     private int codigoItemVenda;
+    @Column(name="quantidade")
     private int quantidade;
+    @Column(name="totalItem")
     private double total;
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name ="dataAlteracao")
+    private Calendar dataItemVenda;
     
    
             
--- sistemasRoupas/src/main/java/com/tads4/sistemasroupas/model/Venda.java
+++ sistemasRoupas/src/main/java/com/tads4/sistemasroupas/model/Venda.java
@@ -5,11 +5,17 @@
  */
 package com.tads4.sistemasroupas.model;
 
+import java.io.Serializable;
 import java.util.Calendar;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
 
 
 /**
@@ -18,14 +24,66 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "Venda")
-public class Venda {
-    @codigoVenda 
+public class Venda implements Serializable{
+    @Id
     @GeneratedValue
-    @Column(name=codigoVenda)
+    @Column(name="codigoVenda")   
     private long codigoVenda;
-    @TemporalType.TIMESTAMP
-    @Column(name=dataVenda)
+    @Temporal(TemporalType.TIMESTAMP )
+    @Column(name="dataVenda")
     private Calendar dataVenda;
+
+    public Venda() {
+    }
+
+    public long getCodigoVenda() {
+        return codigoVenda;
+    }
+
+    public void setCodigoVenda(long codigoVenda) {
+        this.codigoVenda = codigoVenda;
+    }
+
+    public Calendar getDataVenda() {
+        return dataVenda;
+    }
+
+    public void setDataVenda(Calendar dataVenda) {
+        this.dataVenda = dataVenda;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 41 * hash + (int) (this.codigoVenda ^ (this.codigoVenda >>> 32));
+        hash = 41 * hash + (this.dataVenda != null ? this.dataVenda.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Venda other = (Venda) obj;
+        if (this.codigoVenda != other.codigoVenda) {
+            return false;
+        }
+        if (this.dataVenda != other.dataVenda && (this.dataVenda == null || !this.dataVenda.equals(other.dataVenda))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Venda{" + "codigoVenda=" + codigoVenda + ", dataVenda=" + dataVenda + '}';
+    }
+
+    
     
     
     
