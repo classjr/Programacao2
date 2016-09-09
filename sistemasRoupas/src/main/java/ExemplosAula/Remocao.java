747db412a01679cdd7b43a30d881144ad66fb2c5
junior.silva <estrutura.pilha@gmail.com>
2016-09-09 20:10:47 -0300

    Atualizando com novos arquivos de acesso ao banco de dados relacional.

---
+++ sistemasRoupas/src/main/java/ExemplosAula/Remocao.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ExemplosAula;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author junior
+ * Esta classe é responsável pela remoção de dados
+ */
+public class Remocao {
+    private ConexaoDBRelacional conn = null;
+    private Connection connection = null;
+    public Remocao(){
+        try{
+            this.conn = new ConexaoDBRelacional();
+            this.connection = this.conn.getConnection();
+        }catch(Exception ex){
+            System.out.println("Problemas ao conectar com o banco. "+ex.getMessage());
+        }
+    }
+    public void running() throws SQLException{
+        String sql = "delete from pessoa where pessoa.nome = ?";
+        PreparedStatement ps = this.connection.prepareStatement(sql);
+        ps.setString(1, "Juliana");
+        ps.execute();
+        ps.close();
+        this.connection.close();
+    }
+}
--- sistemasRoupas/src/main/java/com/tads4/sistemasroupas/App.java
+++ sistemasRoupas/src/main/java/com/tads4/sistemasroupas/App.java
@@ -1,5 +1,6 @@
 package com.tads4.sistemasroupas;
 
+import ExemplosAula.Remocao;
 import com.tads4.sistemasroupas.views.Main;
 
 /**
@@ -10,8 +11,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        Main mySystem = new Main();
-        mySystem.setVisible(true); 
-       
+       /* Main mySystem = new Main();
+        mySystem.setVisible(true); */
+        try{
+            Remocao rm = new Remocao();
+            rm.running();
+        }catch(Exception ex){
+            System.out.println("Problemas ao remover linha de banco de dados.: "+ex.getMessage());
+        }
     }
 }
