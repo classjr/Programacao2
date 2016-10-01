e0550d36fea6d11cac556115f631a755a1a4e16c
givoane <GiovanePerlin>
2016-10-01 16:29:18 -0400

    Adicionado o método para adicionar Funcionários

--- sistemasRoupas/src/main/java/com/tads4/sistemasroupas/teste/Teste.java
+++ sistemasRoupas/src/main/java/com/tads4/sistemasroupas/teste/Teste.java
@@ -2,12 +2,9 @@ package com.tads4.sistemasroupas.teste;
 
 import com.tads4.sistemasroupas.model.*;
 import com.tads4.sistemasroupas.model.Produto;
+import com.tads4.sistemasroupas.persistence.Crud;
 import java.util.HashSet;
 import java.util.Set;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
 
 /**
  *
@@ -37,6 +34,30 @@ public class Teste {
         return cliente;
     }
 
+    public static Funcionario addFuncionario (int i /*numero de emails/telefones/enderecos*/, String cpf) {
+        Set<Email> emails = new HashSet<Email>();
+        Email[] email = new Email[i];
+        Set<Endereco> enderecos = new HashSet<Endereco>();
+        Endereco[] endereco = new Endereco[i];
+        Set<Telefone> telefones = new HashSet<Telefone>();
+        Telefone[] telefone = new Telefone[i];
+                
+        for (int cont = 0; cont < i; cont++) {
+            email[cont] = new Email("email"+cont+"@email");
+            endereco[cont] = new Endereco("Brasil", "MS", "Nova Andradina", "Centro", "79750-000", "Rua", "complemento", "numero: " + cont);
+            telefone[cont] = new Telefone("numero: " + cont, "67", "operadora");
+            emails.add(email[cont]);
+            enderecos.add(endereco[cont]);
+            telefones.add(telefone[cont]);
+        }
+        
+        Funcionario funcionario = new Funcionario("pwd"+i, "user"+i, 10d, "funcao"+i, emails, enderecos, telefones, "Nome"+i, "00"+i, cpf, 'M', "00/00/000"+i);
+        Crud crud = new Crud();
+        crud.insert(funcionario);
+        
+        return funcionario;
+    }
+    
     /**
      *
      * @param i Número qualquer, para ser concatenado com conteúdo default do
