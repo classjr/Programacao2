9957e16bafb4b0fceeaaba11f35996fe27063001
GiovanePerlin <GiovanePerlin>
2016-10-06 14:48:29 -0400

    Fiz alguns métodos de teste para as Classes que eu estava trabalhando. Arrumei o javadoc dos meus métodos e comentei as linhas do hibernate.cfg.xml que estavam atrapalhando a execução. (Alison, quando for executar descomenta a sua linha no hibernate.cfg.xml e comenta o atributo ItemVenda na sua classe Produto)

--- sistemasRoupas/src/main/java/com/tads4/sistemasroupas/App.java
+++ sistemasRoupas/src/main/java/com/tads4/sistemasroupas/App.java
@@ -6,6 +6,7 @@ import ExemplosAula.hibernate.Loan;
 import ExemplosAula.hibernate.Products;
 import ExemplosAula.hibernate.Provider;
 import ExemplosAula.hibernate.Teacher;
+import com.tads4.sistemasroupas.model.Cliente;
 import com.tads4.sistemasroupas.teste.Teste;
 import java.util.Collection;
 import java.util.Date;
@@ -15,41 +16,56 @@ import java.util.HashSet;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+
+    public static void main(String[] args) {
         /*Main mySystem = new Main();
-        mySystem.setVisible(true); */ 
-        
+        mySystem.setVisible(true); */
+
         //onetomay();
         //manytomany();
         Teste teste = new Teste();
-        //teste.addCliente(0, cpf);
+        //Cliente cliente = teste.addCliente(5, "000.000.000.01");
         //teste.addFornecedor(Integer.SIZE);
         //teste.addFuncionario(0, cpf);
+        //teste.addCliente(0, cpf);
+        //teste.addEmails(0);
+        //teste.addEnderecos(0);
+        //teste.addTelefones(0);
+        //teste.buscaCliente(0);
+        //teste.buscaFuncionario(0);
+        //teste.removeCliente(cliente);
+        //teste.removeCliente(0);
+        //teste.removeEmail("email1@email", cliente.getEmails());
+        //teste.removeEndereco("numero: 1", cliente.getEnderecos());
+        //teste.removeFuncionario(funcionario);
+        //teste.removeFuncionario(0);
+        //teste.removeTelefone("numero: 1", cliente.getTelefones());
+        //teste.atualizarCliente(cliente);
+        //teste.atualizarFuncionario(funcionario);
+
         teste.addProduto("Calca", 50.00, "PP", "Azul", "000.000.120", "rangler");
         //onetoone();
         System.out.println("Salvando no Banco !!!");
-        
+
     }
-    
-    public static void onetomay(){
-        
+
+    public static void onetomay() {
+
         HibernateUtils database = new HibernateUtils();
-        Loan     loan    = new Loan();
-        Teacher  teacher = new Teacher();
+        Loan loan = new Loan();
+        Teacher teacher = new Teacher();
         Collection<Products> products = new HashSet<Products>();
         Collection<Loan> loans = new HashSet<Loan>();
-        
+
         // Criando um objeto emprestimo ****************************************
         loan.setDate(new Date("20/08/2016"));
         loan.setStatus(false);
-        
+
         //Criando lista de produtos ********************************************
         Products pr1 = new Products();
         Products pr2 = new Products();
-        
+
         pr1.setName("Projetor");
         pr1.setColor("black");
         pr1.setModel("small");
@@ -57,7 +73,7 @@ public class App
         pr1.setBarcode("000.000.01");
         pr1.setLoan(loan);
         pr1.setProvider(null);
-        
+
         pr2.setName("spreedPlan");
         pr2.setColor("black");
         pr2.setModel("bigest");
@@ -65,30 +81,30 @@ public class App
         pr2.setBarcode("300.000.21");
         pr2.setLoan(loan);
         pr2.setProvider(null);
-        
+
         products.add(pr2);
         products.add(pr1);
         loan.setProducts(products);
-        
+
         //Criando um usuario para o emprestimo *********************************
         teacher.setDepartament("Ciencia da Computacao");
         loan.setTeacher(teacher);
         loans.add(loan);
-        
+
         teacher.setLoans(loans);
-        
+
         //Salvado no Banco (Na verdade, estamos transformando estes objetos transientes em persistentes)
         database.save(teacher);
         database.close();
-        
+
     }
-    
-    public static void manytomany(){
-        
+
+    public static void manytomany() {
+
         HibernateUtils database = new HibernateUtils();
         Products product = new Products();
         Collection<Provider> providers = new HashSet<Provider>();
-        
+
         //Preenchendo produto **************************************************
         product.setBarcode("400.000.12");
         product.setColor("Green");
@@ -96,53 +112,49 @@ public class App
         product.setModel("bluePrint");
         product.setPrice(100.00);
         product.setName("TableDigital");
-        
-        
+
         //Preenchendo fornecedores *********************************************
         Provider pro1 = new Provider();
         Provider pro2 = new Provider();
-        
+
         pro1.setCnpj("000.0001.0002.000-1");
         pro1.setName("ComercialAndroid");
         pro1.setPhone("+5005634567869");
-        
+
         pro2.setCnpj("222.0001.0442.000-4");
         pro2.setName("FullOptimazed");
         pro2.setPhone("+50056345673547");
-        
-        
+
         providers.add(pro1);
         providers.add(pro2);
-        
+
         product.setProvider(providers);
         //Salvando no Banco de Dados *******************************************
         database.save(pro1);
         database.save(pro2);
-        
+
         database.save(product);
         database.close();
-        
+
     }
-    
-    public static void onetoone(){
-        
+
+    public static void onetoone() {
+
         HibernateUtils database = new HibernateUtils();
         Institute institute = new Institute();
-        Teacher teacher     = new Teacher();
-        
+        Teacher teacher = new Teacher();
+
         //Salvar teacher
         teacher.setDepartament("Ciencias Exatas e da Terra");
         teacher.setLoans(null);
-        
+
         //Salvar institute
         institute.setName("Ciencia da Computacao");
         institute.setTeacher(teacher);
         teacher.setInstitute(institute);
-        
+
         database.save(teacher);
         database.close();
-        
 
-        
     }
 }
--- sistemasRoupas/src/main/java/com/tads4/sistemasroupas/teste/Teste.java
+++ sistemasRoupas/src/main/java/com/tads4/sistemasroupas/teste/Teste.java
@@ -3,6 +3,7 @@ package com.tads4.sistemasroupas.teste;
 import com.tads4.sistemasroupas.model.*;
 import com.tads4.sistemasroupas.model.Produto;
 import com.tads4.sistemasroupas.model.persistence.Crud;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,52 +13,232 @@ import java.util.Set;
  */
 public class Teste {
 
-    public Cliente addCliente(int i /*numero de emails/telefones/enderecos*/, String cpf) {
-        Set<Email> emails = new HashSet<Email>();
-        Email[] email = new Email[i];
-        Set<Endereco> enderecos = new HashSet<Endereco>();
-        Endereco[] endereco = new Endereco[i];
+    /**
+     *
+     * @param i Numero de telefones que serao criados
+     * @return
+     */
+    public Set<Telefone> addTelefones(int i) {
         Set<Telefone> telefones = new HashSet<Telefone>();
-        Telefone[] telefone = new Telefone[i];
+        Telefone telefone = null;
 
         for (int cont = 0; cont < i; cont++) {
-            email[cont] = new Email("email" + cont + "@email");
-            endereco[cont] = new Endereco("Brasil", "MS", "Nova Andradina", "Centro", "79750-000", "Rua", "complemento", "numero: " + cont);
-            telefone[cont] = new Telefone("numero: " + cont, "67", "operadora");
-            emails.add(email[cont]);
-            enderecos.add(endereco[cont]);
-            telefones.add(telefone[cont]);
+            telefone = new Telefone("numero: " + cont, "67", "operadora");
+            telefones.add(telefone);
         }
+
+        return telefones;
+    }
+
+    /**
+     *
+     * @param i Numero de enderecos que serao criados
+     * @return
+     */
+    public Set<Endereco> addEnderecos(int i) {
+        Set<Endereco> enderecos = new HashSet<Endereco>();
+        Endereco endereco = null;
+
+        for (int cont = 0; cont < i; cont++) {
+            endereco = new Endereco("Brasil", "MS", "Nova Andradina", "Centro", "79750-000", "Rua", "complemento", "numero: " + cont);
+            enderecos.add(endereco);
+        }
+
+        return enderecos;
+    }
+
+    /**
+     *
+     * @param i Numero de emails que serao criados
+     * @return
+     */
+    public Set<Email> addEmails(int i) {
+        Set<Email> emails = new HashSet<Email>();
+        Email email = null;
+
+        for (int cont = 0; cont < i; cont++) {
+            email = new Email("email" + cont + "@email");
+            emails.add(email);
+        }
+
+        return emails;
+    }
+
+    /**
+     *
+     * @param i numero de enderecos, emails e telefones que serao criados
+     * (também sera o numero concatenado nos atributos do cliente)
+     * @param cpf numero do cpf do cliente (atributo unico)
+     * @return um Cliente com i enderecos, emails e telefones
+     */
+    public Cliente addCliente(int i /*numero de emails/telefones/enderecos*/, String cpf) {
+        Set<Email> emails = addEmails(i);
+        Set<Endereco> enderecos = addEnderecos(i);
+        Set<Telefone> telefones = addTelefones(i);
 
         Cliente cliente = new Cliente(10d, "00/00/000" + i, "Nome" + i, "00" + i, cpf, 'M', "00/00/000" + i, emails, enderecos, telefones);
         Crud crud = new Crud();
         crud.insert(cliente);
-        
+
         return cliente;
     }
 
-    public Funcionario addFuncionario(int i /*numero de emails/telefones/enderecos*/, String cpf) {
-        Set<Email> emails = new HashSet<Email>();
-        Email[] email = new Email[i];
-        Set<Endereco> enderecos = new HashSet<Endereco>();
-        Endereco[] endereco = new Endereco[i];
-        Set<Telefone> telefones = new HashSet<Telefone>();
-        Telefone[] telefone = new Telefone[i];
-
-        for (int cont = 0; cont < i; cont++) {
-            email[cont] = new Email("email@email" + cont);
-            endereco[cont] = new Endereco("Brasil", "MS", "Nova Andradina", "Centro", "79750-000", "Rua", "complemento", "numero: " + cont);
-            telefone[cont] = new Telefone("numero: " + cont, "67", "operadora");
-            emails.add(email[cont]);
-            enderecos.add(endereco[cont]);
-            telefones.add(telefone[cont]);
-        }
+    /**
+     *
+     * @param i numero de enderecos, emails e telefones que serao criados
+     * (também sera o numero concatenado nos atributos do funcionario)
+     * @param cpf numero do cpf do cliente (atributo unico)
+     * @return um Funcionario com i enderecos, emails e telefones
+     */
+    public Funcionario addFuncionario(int i, String cpf) {
+        Set<Email> emails = addEmails(i);
+        Set<Endereco> enderecos = addEnderecos(i);
+        Set<Telefone> telefones = addTelefones(i);
 
         Funcionario funcionario = new Funcionario("pwd" + i, "user" + i, 10d, "funcao" + i, emails, enderecos, telefones, "Nome" + i, "00" + i, cpf, 'M', "00/00/000" + i);
         Crud crud = new Crud();
         crud.insert(funcionario);
 
         return funcionario;
+    }
+
+    /**
+     *
+     * @param i codigo do cliente que sera buscado
+     */
+    public Cliente buscaCliente(int i) {
+        Crud crud = new Crud();
+        return (Cliente) crud.searchId(i, Cliente.class);
+    }
+
+    /**
+     *
+     * @param i codigo do funcionario que sera buscado
+     */
+    public Funcionario buscaFuncionario(int i) {
+        Crud crud = new Crud();
+        return (Funcionario) crud.searchId(i, Funcionario.class);
+    }
+
+    /**
+     *
+     * @param cliente recebe o cliente que sera removido
+     */
+    public void removeCliente(Cliente cliente) {
+        Crud crud = new Crud();
+        crud.remove(cliente);
+    }
+
+    /**
+     *
+     * @param i recebe o codigo do cliente que sera removido
+     */
+    public void removeCliente(int i) {
+        Crud crud = new Crud();
+        Cliente cliente = buscaCliente(i);
+        crud.remove(cliente);
+    }
+
+    /**
+     *
+     * @param funcionario recebe o funcionario que sera removido
+     */
+    public void removeFuncionario(Funcionario funcionario) {
+        Crud crud = new Crud();
+        crud.remove(funcionario);
+    }
+
+    /**
+     *
+     * @param i recebe o codigo do funcionario que sera removido
+     */
+    public void removeFuncionario(int i) {
+        Crud crud = new Crud();
+        Funcionario funcionario = buscaFuncionario(i);
+        crud.remove(funcionario);
+    }
+
+    /**
+     *
+     * @param numero recebe o numero do(s) telefone(s) que sera(o) removido(s)
+     * @param telefones recebe o Set de telefones de um
+     * Cliente/Fornecedor/Funcionario
+     * @return
+     */
+    public Set<Telefone> removeTelefone(String numero, Set<Telefone> telefones) {
+       ArrayList<Telefone> telefone_remove = new ArrayList<Telefone>();
+
+        for (Telefone telefone : telefones) {
+            if (telefone.getNumero().equals(numero)) {
+                telefone_remove.add(telefone);
+            }
+        }
+        for (Telefone telefone : telefone_remove) {
+            telefones.remove(telefone);
+        }
+
+        return telefones;
+    }
+
+    /**
+     *
+     * @param numero recebe o numero do(s) endereco(s) que sera(o) removido(s)
+     * @param enderecos recebe o Set de enderecos de um
+     * Cliente/Fornecedor/Funcionario
+     * @return
+     */
+    public Set<Endereco> removeEndereco(String numero, Set<Endereco> enderecos) {
+       ArrayList<Endereco> endereco_remove = new ArrayList<Endereco>();
+
+        for (Endereco endereco : enderecos) {
+            if (endereco.getNumero().equals(numero)) {
+                endereco_remove.add(endereco);            }
+        }
+        for (Endereco endereco : endereco_remove) {
+            enderecos.remove(endereco);
+        }
+
+        return enderecos;
+    }
+
+    /**
+     *
+     * @param endereco_email recebe o email do(s) email(s) que sera(o)
+     * removido(s)
+     * @param emails recebe o Set de emails de um Cliente/Fornecedor/Funcionario
+     * @return
+     */
+    public Set<Email> removeEmail(String endereco_email, Set<Email> emails) {
+        ArrayList<Email> email_remove = new ArrayList<Email>();
+
+        for (Email email : emails) {
+            if (email.getEmail().equals(endereco_email)) {
+                email_remove.add(email);
+            }
+        }
+        for (Email email : email_remove) {
+            emails.remove(email);
+        }
+
+        return emails;
+    }
+
+    /**
+     *
+     * @param cliente recebe o cliente que sera atualizado
+     */
+    public void atualizarCliente(Cliente cliente) {
+        Crud crud = new Crud();
+        crud.update(cliente);
+    }
+
+    /**
+     *
+     * @param funcionario recebe o cliente que sera atualizado
+     */
+    public void atualizarFuncionario(Funcionario funcionario) {
+        Crud crud = new Crud();
+        crud.update(funcionario);
     }
 
     /**
@@ -86,9 +267,10 @@ public class Teste {
         return for1;
 
     }
-    
-    /***
-     * 
+
+    /**
+     * *
+     *
      * @param tipo para gravar o tipo do produto ex: calça jeans, blusa, e etc;
      * @param valor para preço do produto;
      * @param tamanho para gravar as medidas do produto ex: P/M/G...;
@@ -97,8 +279,7 @@ public class Teste {
      * @param marca para gravar marca ex: Nike, Adidas, Hering...
      * @return objeto do tipo Produto.
      */
-    
-    public  void addProduto(String tipo, Double valor, String tamanho, String cor, String codigoBarra, String marca) {
+    public void addProduto(String tipo, Double valor, String tamanho, String cor, String codigoBarra, String marca) {
         Produto produto = new Produto();
         Crud crud = new Crud();
         produto.setTipo(tipo);
@@ -107,8 +288,8 @@ public class Teste {
         produto.setCor(cor);
         produto.setCodigoBarra(codigoBarra);
         produto.setMarca(marca);
-        
+
         crud.insert(produto);
-        
+
     }
 }
