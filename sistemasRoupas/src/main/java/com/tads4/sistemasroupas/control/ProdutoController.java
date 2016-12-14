aa4daf8c37b8c6e498421c6fc43576fe15318ddd
alison <alison@172.16.0.207>
2016-12-14 16:52:44 -0300

    Alterado a tela cadastrarProduto para padrão.

--- sistemasRoupas/src/main/java/com/tads4/sistemasroupas/control/ProdutoController.java
+++ sistemasRoupas/src/main/java/com/tads4/sistemasroupas/control/ProdutoController.java
@@ -1,1 +1,17 @@
+package com.tads4.sistemasroupas.control;
 
+import com.tads4.sistemasroupas.model.Produto;
+
+public class ProdutoController{
+    
+    private Produto produto;
+    
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+}
